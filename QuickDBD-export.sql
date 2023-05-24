c919be43481fd4790bb257791f5541681244cefa
jaredap1995 <jaredaperez1995@gmail.com>
2023-05-24 11:41:57 -0700

    schema

---
+++ QuickDBD-export.sql
@@ -0,0 +1,60 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- -Crowdfunding_DB_Schema
+
+CREATE TABLE "categories" (
+    "category_id" INT   NOT NULL,
+    "category_name" TEXT   NOT NULL,
+    CONSTRAINT "pk_categories" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "subcategories" (
+    "subcategory_id" INT   NOT NULL,
+    "subcategory_name" TEXT   NOT NULL,
+    CONSTRAINT "pk_subcategories" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "campaigns" (
+    "cf_id" INT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" TEXT   NOT NULL,
+    "description" TEXT   NOT NULL,
+    "goal" REAL   NOT NULL,
+    "pledged" REAL   NOT NULL,
+    "outcome" TEXT   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" TEXT   NOT NULL,
+    "currency" TEXT   NOT NULL,
+    "launch_date" TIMESTAMP   NOT NULL,
+    "end_date" TIMESTAMP   NOT NULL,
+    "category_id" INT   NOT NULL,
+    "subcategory_id" INT   NOT NULL,
+    CONSTRAINT "pk_campaigns" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" INT   NOT NULL,
+    "contact_first" VARCHAR(255)   NOT NULL,
+    "contact_last" VARCHAR(255)   NOT NULL,
+    "contact_email" TEXT   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+ALTER TABLE "campaigns" ADD CONSTRAINT "fk_campaigns_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaigns" ADD CONSTRAINT "fk_campaigns_category_id" FOREIGN KEY("category_id")
+REFERENCES "categories" ("category_id");
+
+ALTER TABLE "campaigns" ADD CONSTRAINT "fk_campaigns_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategories" ("subcategory_id");
+
